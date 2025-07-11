62f6bd0e765db4204678ca48d2baf2e95c7c4990
PriyoGhosh02 <priyoghosh02@gmail.com>
2025-07-11 14:08:13 +0600

    finished all design but have some issu respondive releted

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const navs = document.querySelectorAll('.nav-list li');
+const cube = document.querySelector('.box');
+const sections = document.querySelectorAll('.section');
+
 const resumeLists = document.querySelectorAll('.resume-list');
 const resumeBoxes = document.querySelectorAll('.resume-box');
 
@@ -7,7 +11,29 @@ const portfolioBoxes = document.querySelectorAll('.portfolio-box');
 
 
 // navbar actions and all section actions along with cube rotation when navbar is clicked
+navs.forEach((nav,idx) => {
+    nav.addEventListener('click', () => {
+        document.querySelector('.nav-list li.active').classList.remove('active');
+        nav.classList.add('active');
 
+        cube.style.transform=`rotateY(${idx* -90}deg)`;
+
+        document.querySelector('.section.active').classList.remove('active');
+        sections[idx].classList.add('active');
+
+        const array=Array.from(sections);
+        const arrSecs =array.slice(1, -1);
+        arrSecs.forEach(arrSecs => {
+            if(arrSecs.classList.contains('active')){
+                sections[4].classList.add('action-contact');
+            }
+        });
+        if(sections[0].classList.contains('active')){
+            sections[4].classList.remove('action-contact')
+        }
+        });
+
+});
 
 // resume section when clicking tab-list
 resumeLists.forEach((list,idx) => {
@@ -35,4 +61,6 @@ portfolioLists.forEach((list,idx) => {
 
 
 // visibility for contact section when reloading (cube reloading animation)
-
+setTimeout(()=>{
+    sections[4].classList.remove('active');
+}, 1500);
